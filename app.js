064588beb0f82ab7c0ec471745a636bc1bad118a
Anthony Matera <anm326@lehigh.edu>
2024-07-06 22:49:50 -0400

    implement basic functionality, buttons now work

--- app.js
+++ app.js
@@ -1,15 +1,56 @@
-// Put your name and ID here 
+// Anthony Matera anm326 
+
+// author and pub dropdowns only work after the add button is pushed
+// when list is clicked, the title box says "please fill out this field"
+// year has limit values (0 to 2024)
 
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(express.static(
-  path.resolve(__dirname, "public")
-));
+app.use(express.static(path.resolve(__dirname, "public")));
+app.use(bodyParser.json());
 
-// Insert code here
+// Book class to represent a book object
+class Book {
+  constructor(title, author, genre, publisher, year, types) {
+    this.title = title;
+    this.author = author;
+    this.genre = genre;
+    this.publisher = publisher;
+    this.year = year;
+    this.types = types;
+  }
+}
 
-app.listen(3000);
+// Array to store books
+let bookList = [];
+let authors = new Set();
+let publishers = new Set();
 
+// Route to add a new book
+app.post("/addBook", (req, res) => {
+  const { title, author, genre, publisher, year, types } = req.body;
+  const newBook = new Book(title, author, genre, publisher, year, types);
+  bookList.push(newBook);
+  
+  if (!authors.has(author)) {
+    authors.add(author);
+  }
+  if (!publishers.has(publisher)) {
+    publishers.add(publisher);
+  }
+  
+  res.json({ authors: Array.from(authors), publishers: Array.from(publishers) });
+});
+
+// Route to get all books
+app.get("/getBooks", (req, res) => {
+  res.json(bookList);
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,8 +1,71 @@
-
 function init() {
-  // Put initialization code here
+  $("#bookForm").on("submit", addBook);
+  $("#listButton").on("click", listBooks);
+  $("#authorDropdown").on("change", function() {
+    $("#author").val($(this).val());
+  });
+  $("#publisherDropdown").on("change", function() {
+    $("#publisher").val($(this).val());
+  });
+  updateDropdowns();
 }
 
-$( () => {
+function addBook(e) {
+  e.preventDefault();
+  const book = {
+    title: $("#title").val(),
+    author: $("#author").val(),
+    genre: $("#genre").val(),
+    publisher: $("#publisher").val(),
+    year: $("#year").val(),
+    types: $("input[type=checkbox]:checked").map(function() {
+      return $(this).val();
+    }).get()
+  };
+
+  $.ajax({
+    url: "/addBook",
+    method: "POST",
+    contentType: "application/json",
+    data: JSON.stringify(book),
+    success: function(response) {
+      updateDropdowns(response.authors, response.publishers);
+      $("#bookForm")[0].reset();
+    }
+  });
+}
+
+function updateDropdowns(authors = [], publishers = []) {
+  $("#authorDropdown").html("<option value=''>Select Author</option>" + 
+    authors.map(author => `<option value="${author}">${author}</option>`).join(""));
+  $("#publisherDropdown").html("<option value=''>Select Publisher</option>" + 
+    publishers.map(publisher => `<option value="${publisher}">${publisher}</option>`).join(""));
+}
+
+function listBooks() {
+  $.ajax({
+    url: "/getBooks",
+    method: "GET",
+    success: function(books) {
+      let tableBody = "";
+      books.forEach(book => {
+        tableBody += `
+          <tr>
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.genre}</td>
+            <td>${book.publisher}</td>
+            <td>${book.year}</td>
+            <td>${book.types.join(", ")}</td>
+          </tr>
+        `;
+      });
+      $("#bookList tbody").html(tableBody);
+    }
+  });
+}
+
+
+$(() => {
   init();
 });
